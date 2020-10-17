9dda5916577625bf4a661527b2a9ba1953f6fdfe
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-10-18 00:49:07 +0600

    state added

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -3,8 +3,16 @@ import React ,{Component} from 'react';
 import './profile'
 import Index from './profile';
 class App extends Component{
+    state={
+        person:[
+            {
+        name:'ringku',
+        age:23
+            }
+        ]
+    }
     render(){
-        return <h5>hhhhhh</h5>
+    return <h5>Hi {this.state.person[0].name} Your age {this.state.person[0].age}</h5>
     }
 }
 export default App
