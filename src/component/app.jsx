0132ba045db1bc193d0aef9bc51aa4043289b861
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-10-22 17:01:49 +0600

    form added

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -13,10 +13,23 @@ class App extends Component{
         [event.target.name]:event.target.value
     })
     }
+    handleSubmit=event=>{
+       event.preventDefault();
+       console.log(this.state)
+       this.setState({
+                name:'',
+                age:''
+          })
+       event.target.reset();
+    }
     render(){
     return <div className="container">
-        <input className="form-control col-md-4" type="text" name="name" onChange={this.handleChange} value={this.state.name} placeholder="Name"/>
-        <button className="btn btn-info mt-2" onClick={()=>console.log(this.state)}>Show</button>
+   <form action="form-group" onSubmit={this.handleSubmit}>
+       <h4 className="text-info">React-Form</h4>
+   <input className="form-control col-md-4" type="text" name="name" onChange={this.handleChange} value={this.state.name} placeholder="Name"/>
+   <input className="form-control col-md-4" type="text" name="age" onChange={this.handleChange}  value={this.state.age} placeholder="Age:24"/>
+        <button type="submit" className="btn btn-info mt-2" >Show</button>
+   </form>
     </div>
     }
 }
